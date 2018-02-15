29cac6489f9daab92a9b6a1eb0a5bf2a397a0a21
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-02-15 19:30:51 +0100

    Add generated code

---
+++ giscube_admin_dockwidget.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+"""
+/***************************************************************************
+ GiscubeAdminDockWidget
+                                 A QGIS plugin
+ A graphical Giscube administration tool
+                             -------------------
+        begin                : 2018-02-15
+        git sha              : $Format:%H$
+        copyright            : (C) 2018 by Martí Angelats i Ribera
+        email                : marti.angelats@gmail.com
+ ***************************************************************************/
+
+/***************************************************************************
+ *                                                                         *
+ *   This program is free software; you can redistribute it and/or modify  *
+ *   it under the terms of the GNU General Public License as published by  *
+ *   the Free Software Foundation; either version 2 of the License, or     *
+ *   (at your option) any later version.                                   *
+ *                                                                         *
+ ***************************************************************************/
+"""
+
+import os
+
+from PyQt5 import QtGui, QtWidgets, uic
+from PyQt5.QtCore import pyqtSignal
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'giscube_admin_dockwidget_base.ui'))
+
+
+class GiscubeAdminDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
+
+    closingPlugin = pyqtSignal()
+
+    def __init__(self, parent=None):
+        """Constructor."""
+        super(GiscubeAdminDockWidget, self).__init__(parent)
+        # Set up the user interface from Designer.
+        # After setupUI you can access any designer object by doing
+        # self.<objectname>, and you can use autoconnect slots - see
+        # http://qt-project.org/doc/qt-4.8/designer-using-a-ui-file.html
+        # #widgets-and-dialogs-with-auto-connect
+        self.setupUi(self)
+
+    def closeEvent(self, event):
+        self.closingPlugin.emit()
+        event.accept()
