2b87fbfa79b702de3974674265b49d96a294af3a
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-02-16 22:32:44 +0100

    Change plugin scripts' DocString

--- giscube_admin.py
+++ giscube_admin.py
@@ -1,25 +1,8 @@
 # -*- coding: utf-8 -*-
 """
-/***************************************************************************
- GiscubeAdmin
-                                 A QGIS plugin
- A graphical Giscube administration tool
-                              -------------------
-        begin                : 2018-02-15
-        git sha              : $Format:%H$
-        copyright            : (C) 2018 by Martí Angelats i Ribera
-        email                : marti.angelats@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+This script contains GiscubeAdmin: the plugin's main class.
 """
+
 from PyQt5.QtCore import QSettings, QTranslator, qVersion, QCoreApplication, Qt
 from PyQt5.QtGui import QIcon
 from PyQt5.QtWidgets import QAction
--- giscube_admin_dockwidget.py
+++ giscube_admin_dockwidget.py
@@ -1,24 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-/***************************************************************************
- GiscubeAdminDockWidget
-                                 A QGIS plugin
- A graphical Giscube administration tool
-                             -------------------
-        begin                : 2018-02-15
-        git sha              : $Format:%H$
-        copyright            : (C) 2018 by Martí Angelats i Ribera
-        email                : marti.angelats@gmail.com
- ***************************************************************************/
-
-/***************************************************************************
- *                                                                         *
- *   This program is free software; you can redistribute it and/or modify  *
- *   it under the terms of the GNU General Public License as published by  *
- *   the Free Software Foundation; either version 2 of the License, or     *
- *   (at your option) any later version.                                   *
- *                                                                         *
- ***************************************************************************/
+This script contains GiscubeAdminDockWidget: the class of the lateral dock that opens with the plugin.
 """
 
 import os
