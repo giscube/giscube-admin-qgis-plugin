3227a6506a291d68d8749c1561d459bbc759245f
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-02-19 23:17:16 +0100

    Bug fixes and make PEP8 compliant

--- backend/giscube_requests.py
+++ backend/giscube_requests.py
@@ -1,41 +1,42 @@
 #!/usr/bin/env python
 """
-Package containing all the classes and utilities to communicate with the Giscube
-server.
+Package containing all the classes and utilities to communicate with the
+Giscube server.
 """
 
 
 class BadCredentials(ConnectionError):
-	"""
-	The credentials or the token used are wrong or have expired.
-	"""
-	pass
+    """
+    The credentials or the token used are wrong or have expired.
+    """
+    pass
 
-# TODO do all the https requests
-class GiscubeRequests:
-	"""
-	Handles all the requests to the Giscube server. May return a BadCredentials
-	error.
-	"""
-	def __init__(self, server_ip, token_handler):
-		self.__ip = server_ip
-		self.__token_handler = token_handler
 
-	def requestProjectsList(self):
-		"""
-		Returns a list with all the projects. May return a BadCredentials error.
-		"""
-		return []
+class GiscubeRequests:  # TODO do all the https requests
+    """
+    Handles all the requests to the Giscube server. May return a BadCredentials
+    error.
+    """
+    def __init__(self, server_ip, token_handler):
+        self.__ip = server_ip
+        self.__token_handler = token_handler
 
-	def requestProject(self, project_name):
-		"""
-		Downloads the project file and returns its path.
-		"""
-		return path
+    def requestProjectsList(self):
+        """
+        Returns a list with all the projects. May return a BadCredentials
+        error.
+        """
+        return []
 
-	def pushProject(self, path, project_name):
-		"""
-		Saves the project in a path to the server with project_name overriding
-		it if exists. May return a BadCredentials error.
-		"""
-		pass
+    def requestProject(self, project_name):
+        """
+        Downloads the project file and returns its path.
+        """
+        return path
+
+    def pushProject(self, path, project_name):
+        """
+        Saves the project in a path to the server with project_name overriding
+        it if exists. May return a BadCredentials error.
+        """
+        pass
--- backend/token_handler.py
+++ backend/token_handler.py
@@ -5,149 +5,145 @@ the server tokens.
 """
 
 from urllib.parse import urljoin
+import json
 
 import requests
 import keyring
 
+
 class TokenHandler:
-	"""
-	Acquires (requests to the server), saves (safely) and refreshes a token.
-	"""
+    """
+    Acquires (requests to the server), saves (safely) and refreshes a token.
+    """
 
-	# TODO check Giscube OAuth parameters
-	GISCUBE_OAUTH_PATH = 'oauth/'
-	GISCUBE_OAUTH_REFRESH_PATH = 'oauth/refresh/'
-	GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS = 302
+    # TODO check Giscube OAuth parameters
+    GISCUBE_OAUTH_PATH = 'oauth/'
+    GISCUBE_OAUTH_REFRESH_PATH = 'oauth/refresh/'
+    GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS = 302
 
-	KEYRING_APP_NAME = "giscube-admin-qgis-plugin"
-	KEYRING_TOKEN_KEY = "token"
-	KEYRING_REFRESH_TOKEN_KEY = "refresh-token"
+    KEYRING_APP_NAME = "giscube-admin-qgis-plugin"
+    KEYRING_TOKEN_KEY = "token"
+    KEYRING_REFRESH_TOKEN_KEY = "refresh-token"
 
-	def __init__(self, server_url):
-		"""
-		Contructor. Loads the saved tokens.
-		"""
-		self.__loadTokens()
-		self._server_url = server_url
+    def __init__(self, server_url):
+        """
+        Contructor. Loads the saved tokens.
+        """
+        self.__loadTokens()
+        self._server_url = server_url
 
-	@property
-	def server_url(self):
-		"""
-		Gets the URL of the credentials of the server.
-		"""
-		return self._server_url
+    @property
+    def server_url(self):
+        """
+        Gets the URL of the credentials of the server.
+        """
+        return self._server_url
 
-	@property
-	def token(self):
-		"""
-		Get the current token.
-		"""
-		return self.__token
+    @property
+    def token(self):
+        """
+        Get the current token.
+        """
+        return self.__token
 
+    def hasToken(self):
+        """
+        Does it have a token?
+        """
+        return self.__token is not None
 
+    def hasRefreshToken(self):
+        """
+        Does it have a refresh token?
+        """
+        return self.__refreshToken is not None
 
-	def hasToken():
-		"""
-		Does it have a token?
-		"""
-		return self.__token != None
+    def refreshToken(self):
+        """
+        Refreshes the token with the refresh token. Returns if it succeded.
+        """
+        if not self.hasRefreshToken():
+            return False
 
+        # TODO check POST data
+        response = requests.post(
+            urljoin(self._server_url, self.GISCUBE_OAUTH_REFRESH_PATH),
+            data={'refresh-token': self.refresh_token})
+        if response == self.GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS:
+            return False
 
+        response.raise_for_status()
+        response_object = json.load(response.content)
 
-	def hasRefreshToken(self):
-		"""
-		Does it have a refresh token?
-		"""
-		return self.__refreshToken != None
+        token = response_object['token']
+        refreshToken = response_object['refresh-token']
 
+        self.__token = token
+        self.__refreshToken = refreshToken
+        self.__saveTokens()
 
+        return True
 
-	def refreshToken(self):
-		"""
-		Refreshes the token with the refresh token. Returns if it succeded.
-		"""
-		if not self.hasRefreshToken():
-			return False
+    def requestNewToken(self, user, password):
+        """
+        Requests a new token to the server. Returns if succeded.
+        It will raise a requests.exceptions.HTTPError if the server responses
+        with an error status code.
+        """
 
-		# TODO check POST data
-		response = requests.post(
-			urljoin(self._server_url, GISCUBE_REFRESH_OAUTH_PATH),
-			data={'refresh-token': self.refresh_token})
-		if response == GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS:
-			return False
+        # TODO check POST data
+        response = requests.post(
+            urljoin(self._server_url, self.GISCUBE_OAUTH_PATH),
+            data={'user': user, 'password': password})
+        if response == self.GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS:
+            return False
+        response.raise_for_status()
+        response_object = json.load(response.content)
 
-		response.raise_for_status()
-		response_object = json.load(response.content)
+        if 'token' in response_object:
+            token = response_object['token']
+        else:
+            return False
 
-		token = response_object['token']
-		refreshToken = response_object['refresh-token']
+        refreshToken = None
+        if 'refresh-token' in response_object:
+            refreshToken = response_object['refresh-token']
 
-		self.__token = token
-		self.__refreshToken = refreshToken
-		self.__saveTokens()
+        self.__token = token
+        self.__refreshToken = refreshToken
+        self.__saveTokens()
 
-		return True
+        return True
 
+    def __loadTokens(self):
+        """
+        Loads the tokens in a safe place.
+        """
+        self.token = keyring.get_password(
+            self.KEYRING_APP_NAME,
+            self.KEYRING_TOKEN_KEY)
+        self.refresh_token = keyring.get_password(
+            self.KEYRING_APP_NAME,
+            self.KEYRING_REFRESH_TOKEN_KEY)
 
+    def __saveTokens(self):
+        """
+        Saves the tokens in a safe place.
+        """
+        keyring.delete_password(
+            self.KEYRING_APP_NAME,
+            self.KEYRING_TOKEN_KEY)
+        keyring.delete_password(
+            self.KEYRING_APP_NAME,
+            self.KEYRING_REFRESH_TOKEN_KEY)
 
-	def requestNewToken(self, user, password):
-		"""
-		Requests a new token to the server. Returns if succeded.
-		It will raise a requests.exceptions.HTTPError if the server responses
-		with an error status code.
-		"""
-
-		# TODO check POST data
-		response = requests.post(
-			urljoin(self._server_url, GISCUBE_OAUTH_PATH),
-			data={'user': user, 'password': password})
-		if response == GISCUBE_OAUTH_BAD_CREDENTIALS_STATUS:
-			return False
-		response.raise_for_status()
-		response_object = json.load(response.content)
-
-		if 'token' in response_object:
-			token = response_object['token']
-		else:
-			return False
-
-		refreshToken = None
-		if 'refresh-token' in response_object:
-			refreshToken = response_object['refresh-token']
-
-		self.__token = token
-		self.__refreshToken = refreshToken
-		self.__saveTokens()
-
-		return True
-
-
-
-	def __loadTokens(self):
-		"""
-		Loads the tokens in a safe place.
-		"""
-		self.token = keyring.get_password(KEYRING_APP_NAME, KEYRING_TOKEN_KEY)
-		self.refresh_token = keyring.get_password(
-			KEYRING_APP_NAME,
-			KEYRING_REFRESH_TOKEN_KEY)
-
-
-
-	def __saveTokens(self):
-		"""
-		Saves the tokens in a safe place.
-		"""
-		keyring.delete_password(KEYRING_APP_NAME, KEYRING_TOKEN_KEY)
-		keyring.delete_password(KEYRING_APP_NAME, KEYRING_REFRESH_TOKEN_KEY)
-
-		if self.token is not None:
-			keyring.set_password(
-				KEYRING_APP_NAME,
-				KEYRING_TOKEN_KEY,
-				self.token)
-		if self.refresh_token is not None:
-			keyring.set_password(
-				KEYRING_APP_NAME,
-				KEYRING_REFRESH_TOKEN_KEY,
-				self.refresh_token)
+        if self.token is not None:
+            keyring.set_password(
+                self.KEYRING_APP_NAME,
+                self.KEYRING_TOKEN_KEY,
+                self.token)
+        if self.refresh_token is not None:
+            keyring.set_password(
+                self.KEYRING_APP_NAME,
+                self.KEYRING_REFRESH_TOKEN_KEY,
+                self.refresh_token)
