e19797eeb60e5f6d656488dff57c36d0d4946e7c
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-02-23 19:45:07 +0100

    Automatic package installation

--- __init__.py
+++ __init__.py
@@ -1,7 +1,32 @@
 # -*- coding: utf-8 -*-
 """
-This script initializes the plugin, making it known to QGIS.
+This script installs, if needed, all the dependencies and then initializes the
+plugin, making it known to QGIS.
 """
+# Importing all the plugin
+from os import path
+from importlib import import_module
+
+DEPENDENCIES = []
+
+try:
+    import pip
+except:
+    exec(open(path.join(path.dirname(__file__), 'get-pip.py').read()))
+    import pip
+    # just in case the included version is old
+    pip.main(['install', '--upgrade', 'pip'])
+
+
+def install_dependency(name):
+    try:
+        import_module(name)
+    except:
+        pip.main(['install', '--upgrade', name])
+
+
+for name in DEPENDENCIES:
+    install_dependency(name)
 
 
 # noinspection PyPep8Naming
@@ -11,6 +36,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :param iface: A QGIS interface instance.
     :type iface: QgsInterface
     """
-    #
     from .giscube_admin import GiscubeAdmin
     return GiscubeAdmin(iface)
