ef38481144c4ae8f13b224288c2cdda780b11104
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-03-01 17:15:41 +0100

    Move contants to a seperate file

---
+++ test/backend/constants.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+"""
+File that contains the list of constants of the test for the backend.
+"""
+
+
+class Test:
+    # TODO: Add mock configuration and credentials
+    URL = 'http://localhost:1234'
+    CLIENT_ID = ''
+    USER = 'user'
+    PASSWORD = 'password'
--- test/backend/token_handler_test.py
+++ test/backend/token_handler_test.py
@@ -5,33 +5,29 @@ Test units for the package backend.token_handler.
 
 from unittest import TestCase
 
+from .constants import Test
 from backend.token_handler import TokenHandler
 
 
 class TestTokenHandler(TestCase):
-    # TODO: Add mock configuration and credentials
-    TEST_URL = 'http://localhost:1234'
-    TEST_CLIENT_ID = ''
-    TEST_USER = 'user'
-    TEST_PASSWORD = 'password'
 
     def test_properties(self):
-        handler = TokenHandler(self.TEST_URL, self.TEST_CLIENT_ID, False)
-        self.assertEqual(handler.server_url, self.TEST_URL)
-        self.assertEqual(handler.client_id, self.TEST_CLIENT_ID)
+        handler = TokenHandler(Test.URL, Test.CLIENT_ID, False)
+        self.assertEqual(handler.server_url, Test.URL)
+        self.assertEqual(handler.client_id, Test.CLIENT_ID)
 
         self.assertFalse(handler.has_access_token)
         self.assertIsNone(handler.access_token)
         self.assertFalse(handler.has_refresh_token)
 
     def test_get_refresh(self):
-        handler = TokenHandler(self.TEST_URL, self.TEST_CLIENT_ID, False)
+        handler = TokenHandler(Test.URL, Test.CLIENT_ID, False)
 
         self.assertFalse(handler.has_access_token)
         self.assertIsNone(handler.access_token)
         self.assertFalse(handler.has_refresh_token)
 
-        handler.login(self.TEST_USER, self.TEST_PASSWORD)
+        handler.login(Test.USER, Test.PASSWORD)
 
         self.assertTrue(handler.has_access_token)
         self.assertIsNotNone(handler.access_token)
@@ -49,17 +45,17 @@ class TestTokenHandler(TestCase):
 
     def test_saving_loading(self):
         handler = TokenHandler(
-            self.TEST_URL,
-            self.TEST_CLIENT_ID,
+            Test.URL,
+            Test.CLIENT_ID,
             True,
             TokenHandler.KEYRING_APP_NAME+'-test')
-        handler.login(self.TEST_USER, self.TEST_PASSWORD)
+        handler.login(Test.USER, Test.PASSWORD)
         old_access_token = handler.access_token
         del handler
 
         handler = TokenHandler(
-            self.TEST_URL,
-            self.TEST_CLIENT_ID,
+            Test.URL,
+            Test.CLIENT_ID,
             True,
             TokenHandler.KEYRING_APP_NAME+'-test')
         self.assertEqual(handler.access_token, old_access_token)
@@ -68,8 +64,8 @@ class TestTokenHandler(TestCase):
         del handler
 
         handler = TokenHandler(
-            self.TEST_URL,
-            self.TEST_CLIENT_ID,
+            Test.URL,
+            Test.CLIENT_ID,
             True,
             TokenHandler.KEYRING_APP_NAME+'-test')
 
