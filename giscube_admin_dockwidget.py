210a5605bbef1d4ad2fd6053e1cd78f001828a71
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-03-14 00:15:16 +0100

    Remove all code (prepare to make new)

--- giscube_admin_dockwidget.py
+++ giscube_admin_dockwidget.py
@@ -24,12 +24,6 @@ class GiscubeAdminDockWidget(QtWidgets.QDockWidget, FORM_CLASS):
         # Set up the user interface from Designer.
         self.setupUi(self)
 
-        # Add signals and slots connections
-        self.loginSubmit.clicked.connect(self.__login)
-
     def closeEvent(self, event):
         self.closingPlugin.emit()
         event.accept()
-
-    def __login(self):
-        pass  # TODO actual login
