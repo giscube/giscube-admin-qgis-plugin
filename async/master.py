e286bd6db0b3bc7aabe36d4d7339a62925527c76
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-03-19 02:38:38 +0100

    Better visibility of the members.

--- async/master.py
+++ async/master.py
@@ -17,42 +17,65 @@ class Master:
         """
         Contructor.
         """
-        self.mutex = QMutex()
-        self.max_slaves = max_slaves
-        self.__slaves = []
-        self.__free_slaves = []
-        self.free_mutex = QMutex()
+        self._max_slaves = max_slaves
+        self._slaves = []
+        self._free_slaves = []
 
-        self.__jobs = []
+        self._jobs = []
+
+        self._mutex = QMutex()
+        self._free_mutex = QMutex()
+
+    @property
+    def max_slaves(self):
+        self._mutex.lock()
+        r = self.__max_slaves
+        self._mutex.unlock()
+
+        return r
+
+    @max_slaves.setter
+    def max_slaves(self, v):
+        self._mutex.lock()
+        self.__max_slaves = v
+        self._mutex.unlock()
 
     def list_job(self, job):
         """
         Lists a job that a slave can work with.
         """
-        self.mutex.lock()
-        if len(self.__slaves) < self.max_slaves:
+        self._mutex.lock()
+        if len(self._slaves) < self._max_slaves:
                 slave = Slave(self, job)
-                self.__slaves.append(slave)
+                self._slaves.append(slave)
                 slave.start()
         else:
             self.__add(job)
-        self.mutex.unlock()
+        self._mutex.unlock()
 
     def aquire_job(self, slave):
         """
         Aquires a job to work with.
         """
-        self.mutex.lock()
-        if len(self.__jobs) > 0:
-            job = self.__jobs.pop()
+        self._mutex.lock()
+        if len(self._jobs) > 0:
+            job = self._jobs.pop()
+            self._mutex.unlock()
         else:
             job = None
-            for i, s in enumerate(self.__slaves):
+
+            # The slave will be free after this (no more jobs to be done).
+            # We can no longer use it but we have to keep an instance until
+            #  it is free to prevent the garbage collector to destroy it too
+            #  soon. So we move it to another list.
+            for i, s in enumerate(self._slaves):
                 if s is slave:
-                    self.free_mutex.lock()
-                    self.__free_slaves.append(self.__slaves.pop(i))
-                    self.free_mutex.unlock()
-        self.mutex.unlock()
+                    self._slaves.pop(i)
+                    self._mutex.unlock()
+
+                    self._free_mutex.lock()
+                    self._free_slaves.append(slave)
+                    self._free_mutex.unlock()
 
         return job
 
@@ -60,16 +83,16 @@ class Master:
         """
         Informs the pool that it lost a slave.
         """
-        self.free_mutex.lock()
-        for i, s in enumerate(self.__free_slaves):
+        self._free_mutex.lock()
+        for i, s in enumerate(self._free_slaves):
             if s is slave:
-                self.__free_slaves.pop(i)
-        self.free_mutex.unlock()
+                self._free_slaves.pop(i)
+        self._free_mutex.unlock()
 
     def __add(self, job):
-        l = len(self.__jobs)
+        l = len(self._jobs)
         i = 0
-        while i < l and self.__jobs[i].priority < job.priority:
+        while i < l and self._jobs[i].priority < job.priority:
             i += 1
 
-        self.__jobs.insert(i, job)
+        self._jobs.insert(i, job)
