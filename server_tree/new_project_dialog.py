61a1e37b7600733da92634bf404852e2c227202b
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-03-27 13:12:00 +0200

    Start making new project dialog

---
+++ server_tree/new_project_dialog.py
@@ -0,0 +1,69 @@
+"""
+This script contains NewServerDialog.
+"""
+
+import os
+
+from PyQt5 import QtWidgets, uic
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QFileDialog
+
+from qgis.core import QgsProject
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'new_project_dialog_base.ui'))
+
+
+class NewProjectDialog(QtWidgets.QDialog, FORM_CLASS):
+    """
+    Popup dialog that ask for the necessary data to make a new server
+    connection.
+    """
+    def __init__(self, giscube_admin, server=None, parent=None):
+        super().__init__(parent=parent)
+
+        self.setupUi(self)
+
+        self.giscube_admin = giscube_admin
+
+        server_names = giscube_admin.server_names()
+
+        # Add all the servers to the lsit and select ours
+        self.servers.clear()
+        self.server.addItems(server_names)
+        index = self.servers.findData(server)
+        self.servers.setCurrentText(index if index >= 0 else 0)
+
+        self.name.setText('New Project')
+
+        def add_current():
+            pass  # TODO
+
+        def add_blank():
+            def open_project():
+                add_current()
+
+            self.iface.newProjectCreated.connect(
+                open_project,
+                Qt.DirectConnection)
+            self.iface.newProject(True)
+
+        def add_file():
+            def open_project():
+                project = QgsProject.instance()
+                dialog = QFileDialog('open')
+                dialog.setFileMode(QFileDialog.ExistingFile)
+                dialog.setFilter("QGIS project (*.qgs)")
+                if dialog.exec_():
+                    path = dialog.selectedFiles()[0]
+                    project.read(path)
+                    add_current()
+
+            self.iface.newProjectCreated.connect(
+                open_project,
+                Qt.DirectConnection)
+            self.iface.newProject(True)
+
+        self.current.clicked.connect(add_current)
+        self.blank.clicked.connect(add_blank)
+        self.file.clicked.connect(add_file)
