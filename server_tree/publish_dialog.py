01ef0f8fd07db74183de057533e7f7b6fff3cfbf
Martí Angelats i Ribera <marti.angelats@gmail.com>
2018-04-20 17:19:52 +0200

    Make publish dialog.

---
+++ server_tree/publish_dialog.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+"""
+This script contains NewServerDialog.
+"""
+
+import os
+
+from PyQt5 import QtWidgets, uic
+
+FORM_CLASS, _ = uic.loadUiType(os.path.join(
+    os.path.dirname(__file__), 'publish_dialog_base.ui'))
+
+
+class PublishDialog(QtWidgets.QDialog, FORM_CLASS):
+    """
+    Popup dialog that ask for the necessary data to publish a project.
+    """
+    def __init__(self, parent=None):
+        super().__init__(parent=parent)
+
+        self.setupUi(self)
+
+    def values(self):
+        values_dic = {
+            'title': self.title.text(),
+            'description': self.descr.text(),
+            'keywords': self.keywords.text(),
+            'visible_on_geoportal': self.visible_on_geoportal.isChecked(),
+        }
+        return values_dic
