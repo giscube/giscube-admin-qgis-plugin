f40e0ac74513e5b3a3fef9ca6121112f34058d30
Manel Clos <manelclos@gmail.com>
2018-05-08 18:33:30 +0200

    windows dependencies

--- __init__.py
+++ __init__.py
@@ -3,6 +3,15 @@
 This script initializes the plugin, making it known to QGIS.
 """
 
+# from geoserverexplorer plugin for QGIS 2.x
+import sys
+import os
+import site
+
+site.addsitedir(
+    os.path.join(os.path.abspath(os.path.dirname(__file__)), 'ext-libs')
+)
+
 
 # noinspection PyPep8Naming
 def classFactory(iface):  # pylint: disable=invalid-name
